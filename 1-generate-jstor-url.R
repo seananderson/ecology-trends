0fac6ebe457e2726e6544d34d10ddc271cb65b51
Sean Anderson <sean@seananderson.ca>
2016-12-09 16:46:23 -0800

    Generate search urls

---
+++ 1-generate-jstor-url.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+decisions <- readr::read_csv("data/journal-filtering.csv")
+
+# if (interactive()) View(d)
+# if (interactive()) View(journals)
+
+include <- filter(decisions, group_vote == "IN", journal != "Ã‰coscience") %>%
+  arrange(journal)
+
+# The format
+# http://www.jstor.org/action/doBasicSearch?Query=jo:%22Journal+of+Animal+Ecology%22+OR+jo:%22Science%22
+
+# If too many search terms search will not complete:
+i <- c(seq(1, nrow(include), 5), nrow(include))
+
+start <- "http://www.jstor.org/action/doBasicSearch?Query=jo:%22"
+end <- "%22"
+
+u <- sapply(seq_len(length(i) - 1), function(x) {
+  main <- paste0(include$journal[i[x]:i[x+1]], collapse = "%22+OR+jo:%22")
+  url <- paste0(start, main, end)
+  url
+})
+
+writeLines(u, "data/generated/urls.txt")
