3c6b7ec038c540d92d18ea47e7f73f82b82fc199
Sean Anderson <sean@seananderson.ca>
2016-12-09 17:23:23 -0800

    Fix URLS and add ecoscience

--- analysis/1-generate-jstor-url.R
+++ analysis/1-generate-jstor-url.R
@@ -4,8 +4,10 @@ decisions <- readr::read_csv("data/journal-filtering.csv")
 # if (interactive()) View(d)
 # if (interactive()) View(journals)
 
-include <- filter(decisions, group_vote == "IN", journal != "Ã‰coscience") %>%
+include <- filter(decisions, group_vote == "IN") %>%
   arrange(journal)
+
+include$journal[include$journal == "Ã‰coscience"] <- "Écoscience"
 
 # The format
 # http://www.jstor.org/action/doBasicSearch?Query=jo:%22Journal+of+Animal+Ecology%22+OR+jo:%22Science%22
@@ -17,7 +19,7 @@ i <- unique(i)
 start <- "http://www.jstor.org/action/doBasicSearch?Query=jo:%22"
 end <- "%22"
 
-u <- sapply(seq_len(length(i) - 1), function(x) {
+u <- sapply(seq(1, length(i), 2), function(x) {
   main <- paste0(include$journal[i[x]:i[x+1]], collapse = "%22+OR+jo:%22")
   url <- paste0(start, main, end)
   url
