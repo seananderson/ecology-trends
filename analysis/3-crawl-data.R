a2fa820f647da46984ca74a366d95cef995648ac
Sean Anderson <sean@seananderson.ca>
2017-06-30 15:50:21 -0700

    Add 2-grams

--- analysis/3-crawl-data.R
+++ analysis/3-crawl-data.R
@@ -47,7 +47,7 @@ db1 <- src_sqlite("data/jstor1.sqlite3", create = TRUE)
 #
 # if (file.exists("data/jstor2.sqlite3"))
 #   file.remove("data/jstor2.sqlite3")
-# db2 <- src_sqlite("data/jstor2.sqlite3", create = TRUE)
+db2 <- src_sqlite("data/jstor2.sqlite3", create = TRUE)
 # copy_to(db2, data.frame(journal = "a", year = 999L, gram = "a", count = 0L,
 #   stringsAsFactors = FALSE), "ngrams", temporary = FALSE)
 #
@@ -62,3 +62,9 @@ insert_data("~/Downloads/receipt-id-334441-jcodes-abcde-part-002", db1, ngram = 
 insert_data("~/Downloads/receipt-id-334441-jcodes-fghijlmnop-part-001", db1, ngram = 1)
 insert_data("~/Downloads/receipt-id-334441-jcodes-fghijlmnop-part-002", db1, ngram = 1)
 insert_data("~/Downloads/receipt-id-334441-jcodes-fghijlmnop-part-003", db1, ngram = 1)
+
+insert_data("~/Downloads/receipt-id-334441-jcodes-abcde-part-001", db2, ngram = 2)
+insert_data("~/Downloads/receipt-id-334441-jcodes-abcde-part-002", db2, ngram = 2)
+insert_data("~/Downloads/receipt-id-334441-jcodes-fghijlmnop-part-001", db2, ngram = 2)
+insert_data("~/Downloads/receipt-id-334441-jcodes-fghijlmnop-part-002", db2, ngram = 2)
+insert_data("~/Downloads/receipt-id-334441-jcodes-fghijlmnop-part-003", db2, ngram = 2)
