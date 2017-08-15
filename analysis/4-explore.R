641283a30d97640eaa83f5748fd4724908efe729
Sean Anderson <sean@seananderson.ca>
2017-08-15 12:02:53 -0700

    Update exploratory script

--- analysis/4-explore.R
+++ analysis/4-explore.R
@@ -99,21 +99,35 @@ ggsave("figs/ecology-group.pdf", width = 10, height = 8)
 
 span <- 0.75
 
-dd <- filter(dat, Group %in% "ecology", year <= 2011, year >= 1935) %>%
+dd <- filter(dat, Group %in% "population_biology", year <= 2011, year >= 1935) %>%
   ungroup() %>%
   group_by(gram) %>%
   mutate(max_count = max(total/total_words))
 
+dd <- group_by(dd, gram) %>%
+  filter(total > 10) %>%
+  mutate(n_year = n()) %>%
+  ungroup() %>%
+  filter(n_year >= 5)
+
 library(mgcv)
+
 dd2 <- plyr::ddply(dd, "gram", function(x) {
-  if (nrow(x) > 10) {
-    m <- gam(I(log10(total/total_words*1000)) ~ s(year), data = x)
-  } else {
-    m <- gam(I(log10(total/total_words*1000)) ~ s(year, k = 3), data = x)
-  }
-  data.frame(year = x$year, pred = predict(m))
+  m <- gam(I(log10(total/total_words*1000)) ~ s(year), data = x)
+  mlm <- lm(I(log10(total/total_words*1000)) ~ year, data = x)
+  y <- seq(min(x$year), max(x$year), 0.5)
+  data.frame(year = y, pred = predict(m, newdata = data.frame(year = y)),
+    slope = coef(mlm)[[2]])
 })
 
+# library(ggrepel)
+
+labs_min <- group_by(dd2, gram) %>%
+  summarise(pred = pred[year == min(year)],
+    slope = slope[year == min(year)], year = min(year))
+labs_max <- group_by(dd2, gram) %>%
+  summarise(pred = pred[year == max(year)],
+    slope = slope[year == max(year)], year = max(year))
 
 ggplot(dd2, aes(year, pred, group = gram, colour = gram)) +
   # geom_point(alpha = 0.1, colour = "grey40", cex = 0.5) +
@@ -122,13 +136,80 @@ ggplot(dd2, aes(year, pred, group = gram, colour = gram)) +
   #   method.args = list(family = Gamma(link = "log")),
   #   formula = y ~ s(x), se = FALSE) +
   theme_sleek() +
-  geom_line() +
+  geom_line(lwd = 0.8) +
+  geom_text_repel(
+    data = labs_min,
+    aes(label = gram),
+    size = 3,
+    nudge_x = -5,
+    segment.size = 0.2,
+    segment.color = "#00000030"
+  ) +
+  geom_text_repel(
+    data = labs_max,
+    aes(label = gram),
+    size = 3,
+    nudge_x = 10,
+    segment.size = 0.2,
+    segment.color = "#00000030"
+  ) +
   # facet_wrap(~fct_reorder(gram, -max_count), scales = "free_y") +
   ylab("log10(Instances per 1000 'ecology')") +
-  ggtitle("ecology") +
+  ggtitle("population_biology") +
   xlim(1925, 2025) +
   # stat_smooth(span=span, se=F) +
-  scale_colour_discrete(guide = 'none') +
+  # scale_colour_discrete(guide = 'none') +
+  # scale_color_gradient2(
+    # low = scales::muted("blue"),
+    # mid = "grey80",
+    # high = scales::muted("red")) +
+  scale_alpha_continuous(limits = c(-20, -2))
   # viridis::scale_colour_viridis(discrete = TRUE) +
-  geom_dl(aes(label = gram), method = list(dl.combine("first.points", "last.points"), cex = 0.7))
-ggsave("figs/ecology-group.pdf", width = 9, height = 8)
+  # geom_dl(aes(label = gram), method = list(dl.combine("first.points", "last.points"), cex = 0.8))
+ggsave("figs/population_biology-group2.pdf", width = 15, height = 12)
+
+# ------
+
+d2 <- dplyr::src_sqlite("data/jstor2.sqlite3")
+temp2 <- dplyr::tbl(d2, "ngrams") %>% filter(year != 999)
+
+eco_grams <- temp2 %>% filter(year == 2008) %>%
+  filter(gram %like% "% ecology") %>%
+  collect(n = Inf)
+
+
+eco_grams2 <- group_by(eco_grams, gram) %>%
+  summarize(n = sum(count)) %>%
+  filter(n > 25) %>%
+  arrange(-n) %>%
+  as.data.frame()
+
+eco_grams2$first <- unlist(lapply(strsplit(eco_grams2$gram, " "), function(x) x[1]))
+
+eco_grams2 <- filter(eco_grams2, !first %in% c("the", "a", "their", "and", "this", "al", "an", "of", "in", "for", "eds", "austral", "side", "one", "tional", "levels", "nities", "or", "usa", "alaska", "ed", "to", "on", "into", "america", "matters")) %>%
+  filter(!grepl("[0-9]", first)) %>%
+  filter(!grepl("^[a-zA-Z]$", first))
+
+write_csv(eco_grams2, path = "ecologies.csv")
+
+make_fig <- function(group) {
+  filter(dat, gram %in% group, year <= 2011, year >= 1935) %>%
+    ungroup() %>%
+    group_by(gram) %>%
+    mutate(max_count = max(total/total_words)) %>%
+    ggplot(aes(year, (total/total_words)*1)) +
+    geom_point(alpha = 0.7, colour = "grey40") +
+    geom_smooth(colour = "red", method = "gam",
+      method.args = list(family = Gamma(link = "log")),
+      formula = y ~ s(x), se = FALSE) +
+    theme_sleek() +
+    facet_wrap(~fct_reorder(gram, -max_count), scales = "free_y") +
+    ylab("Instances per 'ecology'") +
+    ggtitle(group)
+}
+
+# ----------
+
+g <- make_fig(eco_grams$gram)
+ggsave("figs/ecology-group.pdf", width = 10, height = 8)
+
