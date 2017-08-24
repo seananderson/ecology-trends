16ddd0e02d8c3b010fca070fa655548970d4d658
Sean Anderson <sean@seananderson.ca>
2017-08-24 16:25:50 -0700

    Fix duplicate sequence bug; capture all pub_ids this time

--- analysis/3-crawl-data.R
+++ analysis/3-crawl-data.R
@@ -10,8 +10,8 @@ rm(list = ls()) # need all memory possible
 #' @param ngram 1, 2, or 3 for number of words
 #' @param max_n Maximum number of papers to read in at once. This should be as
 #'   big as possible without running out of memory.
-insert_data <- function(path, db, ngram = 1, max_n = 5000L,
-  journal_filter = NULL, save_pub_id = FALSE) {
+insert_data <- function(path, db, ngram = 1, max_n = 6000L,
+  journal_filter = NULL, save_pub_id = TRUE) {
 
   assert_that(dir.exists(path))
   assert_that(is.character(path))
@@ -28,12 +28,12 @@ insert_data <- function(path, db, ngram = 1, max_n = 5000L,
     x <- x[grepl(paste0("^", journal_filter, "\\/"), x)]
     if (length(x) == 0) return(NA)
   }
-  divs <- seq(1, length(x), max_n)
+  divs <- seq(0, length(x), max_n)
 
   if (max(divs) < length(x)) divs <- c(divs, length(x))
   for (i in seq(1, length(divs) - 1)) {
-    message(paste("Extracting", divs[i], "-", divs[i+1], "of", max(divs)))
-    x_temp <- x[divs[i]:divs[i+1]]
+    message(paste("Extracting", (divs[i] + 1), "-", divs[i+1], "of", max(divs)))
+    x_temp <- x[(divs[i] + 1):divs[i + 1]]
     d <- data.frame(
       journal = purrr::map_chr(x_temp, function(a) strsplit(a, "/")[[1]][[1]]),
       stringsAsFactors = FALSE)
@@ -55,15 +55,15 @@ insert_data <- function(path, db, ngram = 1, max_n = 5000L,
 }
 
 db1 <- src_sqlite("data/jstor1.sqlite3", create = TRUE)
-copy_to(db1, data.frame(journal = "a", year = 999L, gram = "a", count = 0L,
+copy_to(db1, data.frame(journal = "a", pub_id = 999L, year = 999L, gram = "a", count = 0L,
   stringsAsFactors = FALSE), "ngrams", temporary = FALSE)
 
 db2 <- src_sqlite("data/jstor2.sqlite3", create = TRUE)
-copy_to(db2, data.frame(journal = "a", year = 999L, gram = "a", count = 0L,
+copy_to(db2, data.frame(journal = "a", pub_id = 999L, year = 999L, gram = "a", count = 0L,
   stringsAsFactors = FALSE), "ngrams", temporary = FALSE)
 
 db3 <- src_sqlite("data/jstor3.sqlite3", create = TRUE)
-copy_to(db3, data.frame(journal = "a", year = 999L, gram = "a", count = 0L,
+copy_to(db3, data.frame(journal = "a", pub_id = 999L, year = 999L, gram = "a", count = 0L,
   stringsAsFactors = FALSE), "ngrams", temporary = FALSE)
 
 f <- list.files("data/raw", full.names = TRUE)
@@ -80,27 +80,28 @@ system.time({
   lapply(f, function(x) insert_data(x, db3, ngram = 3))
 })
 
-# PNAS:
-db1_pnas <- src_sqlite("data/jstor1_pnas.sqlite3", create = TRUE)
-copy_to(db1_pnas, data.frame(journal = "a", pub_id = 999L, year = 999L, gram = "a", count = 0L,
-  stringsAsFactors = FALSE), "ngrams", temporary = FALSE)
-
-db2_pnas <- src_sqlite("data/jstor2_pnas.sqlite3", create = TRUE)
-copy_to(db2_pnas, data.frame(journal = "a", pub_id = 999L, year = 999L, gram = "a", count = 0L,
-  stringsAsFactors = FALSE), "ngrams", temporary = FALSE)
-
-db3_pnas <- src_sqlite("data/jstor3_pnas.sqlite3", create = TRUE)
-copy_to(db3_pnas, data.frame(journal = "a", pub_id = 999L, year = 999L, gram = "a", count = 0L,
-  stringsAsFactors = FALSE), "ngrams", temporary = FALSE)
-
-f <- list.files("data/raw", full.names = TRUE)
-
-system.time({
-  lapply(f[1:3], function(x) insert_data(x, db1_pnas, ngram = 1, journal_filter = "procnatiacadscie", save_pub_id = TRUE))
-})
-system.time({
-  lapply(f, function(x) insert_data(x, db2_pnas, ngram = 2, journal_filter = "procnatiacadscie", save_pub_id = TRUE))
-})
-system.time({
-  lapply(f, function(x) insert_data(x, db3_pnas, ngram = 3, journal_filter = "procnatiacadscie", save_pub_id = TRUE))
-})
+# # PNAS:
+# db1_pnas <- src_sqlite("data/jstor1_pnas.sqlite3", create = TRUE)
+# copy_to(db1_pnas, data.frame(journal = "a", pub_id = 999L, year = 999L, gram = "a", count = 0L,
+#   stringsAsFactors = FALSE), "ngrams", temporary = FALSE)
+#
+# db2_pnas <- src_sqlite("data/jstor2_pnas.sqlite3", create = TRUE)
+# copy_to(db2_pnas, data.frame(journal = "a", pub_id = 999L, year = 999L, gram = "a", count = 0L,
+#   stringsAsFactors = FALSE), "ngrams", temporary = FALSE)
+#
+# db3_pnas <- src_sqlite("data/jstor3_pnas.sqlite3", create = TRUE)
+# copy_to(db3_pnas, data.frame(journal = "a", pub_id = 999L, year = 999L, gram = "a", count = 0L,
+#   stringsAsFactors = FALSE), "ngrams", temporary = FALSE)
+#
+# f <- list.files("data/raw", full.names = TRUE)
+# f <- f[grep("fghijlmnop", f)]
+#
+# system.time({
+#   lapply(f, function(x) insert_data(x, db1_pnas, ngram = 1, journal_filter = "procnatiacadscie", save_pub_id = TRUE))
+# })
+# system.time({
+#   lapply(f, function(x) insert_data(x, db2_pnas, ngram = 2, journal_filter = "procnatiacadscie", save_pub_id = TRUE))
+# })
+# system.time({
+#   lapply(f, function(x) insert_data(x, db3_pnas, ngram = 3, journal_filter = "procnatiacadscie", save_pub_id = TRUE))
+# })
