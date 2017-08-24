1f51169b4f33e1701a63cdde4f98df43e7b946a9
Sean Anderson <sean@seananderson.ca>
2017-08-24 11:41:55 -0700

    PNAS specific data entry

--- analysis/3-crawl-data.R
+++ analysis/3-crawl-data.R
@@ -24,8 +24,10 @@ insert_data <- function(path, db, ngram = 1, max_n = 5000L,
   x <- dir(path, recursive = TRUE, pattern = "*.txt")
   x <- x[grepl(paste0("NGRAMS", ngram, ".txt"), x)]
 
-  if (!is.null(journal_filter))
+  if (!is.null(journal_filter)) {
     x <- x[grepl(paste0("^", journal_filter, "\\/"), x)]
+    if (length(x) == 0) return(NA)
+  }
   divs <- seq(1, length(x), max_n)
 
   if (max(divs) < length(x)) divs <- c(divs, length(x))
@@ -63,15 +65,39 @@ copy_to(db3, data.frame(journal = "a", year = 999L, gram = "a", count = 0L,
 
 f <- list.files("data/raw", full.names = TRUE)
 # Already done:
-f <- f[!f %in% "data/raw/entered"]
+# f <- f[!f %in% "data/raw/entered"]
 
 system.time({
-  lapply(f, function(x) insert_data(x, db1, ngram = 1))#,
-  # journal_filter = "procnatiacadscie"))
+  lapply(f, function(x) insert_data(x, db1, ngram = 1))
 })
 system.time({
   lapply(f, function(x) insert_data(x, db2, ngram = 2))
 })
 system.time({
   lapply(f, function(x) insert_data(x, db3, ngram = 3))
+})
+
+# PNAS:
+db1_pnas <- src_sqlite("data/jstor1_pnas.sqlite3", create = TRUE)
+copy_to(db1_pnas, data.frame(journal = "a", pub_id = 999L, year = 999L, gram = "a", count = 0L,
+  stringsAsFactors = FALSE), "ngrams", temporary = FALSE)
+
+db2_pnas <- src_sqlite("data/jstor2_pnas.sqlite3", create = TRUE)
+copy_to(db2_pnas, data.frame(journal = "a", pub_id = 999L, year = 999L, gram = "a", count = 0L,
+  stringsAsFactors = FALSE), "ngrams", temporary = FALSE)
+
+db3_pnas <- src_sqlite("data/jstor3_pnas.sqlite3", create = TRUE)
+copy_to(db3_pnas, data.frame(journal = "a", pub_id = 999L, year = 999L, gram = "a", count = 0L,
+  stringsAsFactors = FALSE), "ngrams", temporary = FALSE)
+
+f <- list.files("data/raw", full.names = TRUE)
+
+system.time({
+  lapply(f, function(x) insert_data(x, db1_pnas, ngram = 1, journal_filter = "procnatiacadscie"))
+})
+system.time({
+  lapply(f, function(x) insert_data(x, db2_pnas, ngram = 2, journal_filter = "procnatiacadscie"))
+})
+system.time({
+  lapply(f, function(x) insert_data(x, db3_pnas, ngram = 3, journal_filter = "procnatiacadscie"))
 })
